c7ed96571ad9970d15262299a96e8797f201cec6
habul <habul.tekaje@gmail.com>
2022-03-16 16:55:05 +0700

    accept login ui

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -3,81 +3,109 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>HS | Login</title>
 	<link rel='icon' href="<?php echo base_url(); ?>gambar/website/hs-ico.png" type="image/gif">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-	</script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
 		integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
 	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-		integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-	</script>
+	<link rel="stylesheet" type="text/css"
+		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/css/login.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/css/all.min.css">
 </head>
 
-<body class='container'>
-	<div class='row'>
-	</div>
+<body class='hold-transition login-page'>
 	<div class='position-relative'>
-		<div style='background-color:lightgray;'>
-			<div class='container h-100'>
-				<div class='d-flex justify-content-center h-100'>
-					<div class='user_card'>
-						<div class='d-flex justify-content-center'>
-							<div class='brand_logo_container'>
-								<img src='<?php echo base_url(); ?>gambar/website/hs-logo.png' class='brand_logo' alt='Logo'>
-							</div>
+		<div class='container h-100'>
+			<div class='d-flex justify-content-center h-100'>
+				<div class='user_card'>
+					<div class='d-flex justify-content-center'>
+						<div class='brand_logo_container'>
+							<img src='<?php echo base_url(); ?>gambar/website/hs-logo.png' class='brand_logo' alt='Logo'>
 						</div>
-						<div class='d-flex justify-content-center form_container'>
-							<form action='' method='POST'>
-								<div class='input-group mb-3'>
-									<div class='input-group-append'>
-										<span class='input-group-text'><i class='fas fa-user'></i></span>
-									</div>
-									<input type='text' name='hs_user' class='form-control input_user' value=''
-										placeholder='username' required>
-								</div>
-								<div class='input-group mb-2'>
-									<div class='input-group-append'>
-										<span class='input-group-text'><i class='fas fa-key'></i></span>
-									</div>
-									<input type='password' name='hs_pass' class='form-control input_pass' value=''
-										placeholder='password' required>
-								</div>
-								<div class='form-group'>
-									<div class='custom-control custom-checkbox'>
-										<input type='checkbox' class='custom-control-input' id='customControlInline'>
-										<label class='custom-control-label' for='customControlInline'>Remember me</label>
+					</div>
+					<div class='d-flex justify-content-center form_container'>
+						<form action="<?php echo base_url() . 'login/proses' ?>"
+							onsubmit="logbtn.disabled = true; return true;" id="loginform" method="post">
+							<div class="input-group mb-3">
+								<div class="input-group-append">
+									<div class="input-group-text">
+										<span class="fas fa-user">&nbsp;</span>
 									</div>
 								</div>
-								<div class='d-flex justify-content-center mt-3 login_container'>
-									<input type='hidden' name='route' value='route_login'>
-									<input type='submit' name='lsubmit' class='btn login_btn' value='Login'>
+								<input type="text" class="form-control" placeholder="Username" id="username" name="username"
+									required="" oninvalid="this.setCustomValidity('Field username belum diisi')"
+									oninput="setCustomValidity('')" autofocus>
+							</div>
+							<?php echo form_error('username'); ?>
+							<div class="input-group mb-3">
+								<div class="input-group-append">
+									<div class="input-group-text">
+										<span toggle="#password-field" class="fa fa-fw fa-lock field-icon toggle-password"></span>
+									</div>
 								</div>
-							</form>
+								<input id="password-field" type="password" class="form-control" name="password"
+									placeholder="Password" required=""
+									oninvalid="this.setCustomValidity('Field Password belum diisi')"
+									oninput="setCustomValidity('')">
+							</div>
+							<?php echo form_error('password'); ?>
+							<div class='form-group'>
+								<div class='custom-control custom-checkbox'>
+									<input type='checkbox' class='custom-control-input' id='customControlInline'>
+									<label class='custom-control-label' for='customControlInline'>Remember me</label>
+								</div>
+							</div>
+
+							<div class="col-12">
+								<button type="submit" id="logbtn" class="btn btn-danger btn-block">Sign In</button>
+							</div>
+
+						</form>
+					</div>
+					<div class='mt-3'>
+						<div class='d-flex justify-content-center links'>
+							Don't have an account? <a href='#' class='ml-2'>Sign Up</a>
+						</div>
+						<div class='d-flex justify-content-center links'>
+							<a href='#'>Forgot your password?</a>
 						</div>
 					</div>
 				</div>
 			</div>
+			<?php
+		if (isset($_GET['alert'])) {
+			if ($_GET['alert'] == "gagal") {
+			echo "<div class='alert alert-warning font-weight-bold text-center shadow'><i class='icon fas fa-exclamation-triangle'></i>Login Gagal!</div>";
+			} else if ($_GET['alert'] == "belum_login") {
+			echo "<div class='alert alert-danger font-weight-bold text-center shadow'><i class='icon fas fa-ban'></i>Anda Harus Login Terlebih Dulu!</div>";
+			} else if ($_GET['alert'] == "logout") {
+			echo "<div class='alert alert-success font-weight-bold text-center shadow'><i class='icon fas fa-bell'></i>Anda Telah Logout!</div>";
+			} else if ($_GET['alert'] == "registered") {
+			echo "<div class='alert alert-success font-weight-bold text-center shadow'><i class='icon fas fa-bell'></i>User berhasil di tambah</div>";
+			}
+		}
+		?>
 		</div>
 	</div>
-</body>
-<br>
-<br>
-<br>
-<footer>
-	<br>
-	<div class='container'>
-		<div style='font-size:10px;height:100px;overflow:auto;'></div>
-	</div>
-	<script type='text/javascript'>
+	<script>
+		$(document).ready(function () {
+			$(".toggle-password").click(function () {
+				$(this).toggleClass("fa-lock fa-lock-open");
+				var input = $($(this).attr("toggle"));
+				if (input.attr("type") == "password") {
+					input.attr("type", "text");
+				} else {
+					input.attr("type", "password");
+				}
+			});
+		});
+
 		if (window.history.replaceState) {
 			window.history.replaceState(null, null, window.location.href);
 			$(function () {
@@ -86,6 +114,6 @@
 		}
 
 	</script>
-</footer>
+</body>
 
 </html>
